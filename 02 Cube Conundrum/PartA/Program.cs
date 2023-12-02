2f92c44ed9ee059a4731612e23cc27b0fea80e52
Ulf Haueisen <ulf@dvlp.de>
2023-12-02 11:37:24 +0100

    Day 02 Cube Conundrum

---
+++ 02 Cube Conundrum/PartA/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\input.txt");
+            int result = 0;
+            int gamenumber = 1;
+            var limits = new Dictionary<string, int>() { { "blue", 14 }, { "green", 13 }, { "red", 12 } };
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                bool possible = true;
+                foreach (var grab in line!.Split(": ")[1].Split("; "))
+                {
+                    foreach (var col in grab.Split(", "))
+                    {
+                        if (col.Split(" ") is [var vs, var n])
+                        {
+                            int v = int.Parse(vs);
+                            if (v > limits[n])
+                            {
+                                possible = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (!possible) break;
+                }
+                if (possible) result += gamenumber;
+                gamenumber++;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 02 Cube Conundrum/PartB/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\input.txt");
+            int result = 0;
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                var min = new Dictionary<string, int>() { { "blue", 0 }, { "green", 0 }, { "red", 0 } };
+                foreach (var grab in line!.Split(": ")[1].Split("; "))
+                {
+                    foreach (var col in grab.Split(", "))
+                    {
+                        if (col.Split(" ") is [var vs, var n])
+                        {
+                            int v = int.Parse(vs);
+                            if (v > min[n]) min[n] = v;
+                        }
+                    }
+                }
+                result += min["blue"] * min["green"] * min["red"];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
