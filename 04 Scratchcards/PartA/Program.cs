913678f45aafb56c7fb39a6575fab22f3188d99a
Ulf Haueisen <ulf@dvlp.de>
2023-12-04 07:02:32 +0100

    Day 04 Scratchcards

---
+++ 04 Scratchcards/PartA/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\input.txt");
+            int result = 0;
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                var seg = line.Split(new char[] { ':', '|' }, StringSplitOptions.TrimEntries);
+                var elf = seg[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToList();
+                var winning = seg[2].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToList();
+                int points = 0;
+                foreach (var e in elf)
+                {
+                    if (winning.Contains(e))
+                    {
+                        if (points == 0) points = 1; else points *= 2;
+                    }
+                }
+                result += points;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 04 Scratchcards/PartB/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> copies = new Dictionary<int, int>();
+            var sr = new StreamReader(@"..\..\input.txt");
+            int result = 0;
+            int card = 1;
+            while (!sr.EndOfStream)
+            {
+                copies.TryAdd(card, 0);
+                var line = sr.ReadLine();
+                var seg = line.Split(new char[] { ':', '|' }, StringSplitOptions.TrimEntries);
+                var elf = seg[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToList();
+                var winning = seg[2].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToList();
+                int points = 0;
+                foreach (var e in elf)
+                {
+                    if (winning.Contains(e)) points++;
+                }
+                for (int i = 1; i <= points; i++)
+                {
+                    copies.TryAdd(card + i, 0);
+                    copies[card + i] += copies[card] + 1;
+                }
+                card++;
+            }
+            foreach (var c in copies)
+            {
+                if (c.Key < card)
+                {
+                    Console.WriteLine($"{c.Key} {c.Value}");
+                    result += c.Value + 1;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
