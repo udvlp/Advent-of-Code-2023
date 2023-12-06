3bb6db9b2f13cba321c15dcf19bff0b7d309b4cb
Ulf Haueisen <ulf@dvlp.de>
2023-12-06 23:15:23 +0100

    Day 06 Wait For It

---
+++ 06 Wait For It/PartA/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\input.txt");
+            var time = sr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Skip(1).Select(int.Parse).ToList();
+            var dist = sr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Skip(1).Select(int.Parse).ToList();
+            int result = 1;
+            for (int i = 0; i < time.Count; i++)
+            {
+                int t = time[i];
+                int d = dist[i];
+                int c = 0;
+                for (int b = 1; b < t; b++)
+                {
+                    int m = b * (t - b);
+                    if (m > d) c++;
+                }
+                result *= c;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 06 Wait For It/PartB/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\input.txt");
+            var t = long.Parse(sr.ReadLine().Split(':')[1].Replace(" ", ""));
+            var d = long.Parse(sr.ReadLine().Split(':')[1].Replace(" ", ""));
+            long result = 1;
+            int c = 0;
+            for (long b = 1; b < t; b++)
+            {
+                long m = b * (t - b);
+                if (m > d) c++;
+            }
+            result *= c;
+            Console.WriteLine(result);
+        }
+    }
+}
