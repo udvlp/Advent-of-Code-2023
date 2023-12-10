97308ce4f82ca86d7945d820581184380a632df2
Ulf Haueisen <ulf@dvlp.de>
2023-12-10 11:47:08 +0100

    Day 09 Mirage Maintenance

---
+++ 09 Mirage Maintenance/Part2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var sr = new StreamReader(@"..\..\input.txt");
+        long result = 0;
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            var values = line.Split(' ').Select(long.Parse).Reverse().ToList();
+
+            long sequence(List<long> deltas)
+            {
+                Console.WriteLine(String.Join(", ", deltas));
+                if (deltas.All(d => d == 0)) return 0;
+                return deltas[^1] + sequence(deltas.Zip(deltas.Skip(1)).Select(p => p.Second - p.First).ToList());
+            }
+
+            var r = sequence(values);
+            Console.WriteLine(r);
+            result += r;
+
+        }
+        Console.WriteLine($"Result: {result}");
+    }
+}
